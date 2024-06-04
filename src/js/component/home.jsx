60615fe2ae9a929ca1451f709844b3d0ac7e93a3
Javier Seiglie <96433186+jseiglie@users.noreply.github.com>
2024-06-04 11:16:05 +0000

    feat: on item is_done=true item,  has line trhough

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
 	const [todos, setTodos] = useState(); // state that will store the todos
 	const [task, setTask] = useState(""); // state that will hold the new task
 	const [edit, setEdit] = useState(false); //state to active the edit functionality
-	const [editing, setEditing] = useState(); // state that will hold the id of the todo to edit
+	const [editing, setEditing] = useState(false); // state that will hold the id of the todo to edit
 	const [username, setUsername] = useState(); // the username which we will be fetching data from
 	/*
 	METHODS
@@ -51,7 +51,13 @@ const Home = () => {
 
 	const createUser = () => {
 		//we receive the username from the username state
-		fetch("https://playground.4geeks.com/todo/users/" + username)
+		const opt = {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}
+		fetch("https://playground.4geeks.com/todo/users/" + username, opt)
 			.then(resp => resp.json())
 			.then(data => console.log(data))
 			.catch(error => console.log(error))
@@ -155,7 +161,7 @@ const Home = () => {
 		//we will send the label property of the element
 		//------------------------------
 		//checking if the edit state is true
-		if (editing) {
+		
 			fetch("https://playground.4geeks.com/todo/todos/" + el.id, {
 				method: "PUT",
 				headers: {
@@ -164,7 +170,7 @@ const Home = () => {
 				body: JSON.stringify({
 					//if the edited task is empty, we send the old task instead
 					label: task.trim().length == 0 ? el.label : task,
-					is_done: false
+					is_done: el.is_done
 				})
 			})
 				.then(resp => {
@@ -175,32 +181,54 @@ const Home = () => {
 					//we return our edit to a false state
 					setEditing(false)
 					//clean the task state
-					setTask("");
+					setTask('');
 					//clean the task state
 					//after success, we load the data again from the server
 					return loadData();
 				})
 				.catch(error => {
 					//we catch any error for our app not to break if there's an actual error
+					setEditing(false)
 					console.log(error)
 				})
-		}
+		
 	}
 	//function to capitalize the username
 	const capitalizeFirstLetter = (str) => str[0].toUpperCase() + str.slice(1);
 
+	const handleDone = (task, i) => {
+		const aux = todos
+		task.is_done = true
+		// aux[i].is_done = true
+		// setTodos(aux)
+		setEditing(true)
+		handleEdit(task)
+
+	}
+
+	const handleNotDone = (task, i) => {
+		const aux = todos
+		task.is_done = false
+		// aux[i].is_done = false
+		// setTodos(aux)
+		setEditing(true)
+		handleEdit(task)
+
+	}
+
 
 	return (
-		<div className="text-center">
-			<div className="d-flex flex-column ms-auto my-3 w-25 p-3">
+		<div className="text-center d-flex flex-column align-items-center">
+			<h1 className="my-5">Todo List con Fetch!</h1>
+			{!todos && <div className="d-flex flex-column  my-3 p-3 text-center justify-content-center w-50">
 				<p className="form-text">Type your username, if you don't have an account we'll create one.</p>
-				<div className="d-flex">
+				<div className="d-flex flex-column p-2">
 					<input type="text" onChange={e => setUsername(e.target.value)} className="form-control" />
-					<button className="btn btn-success" onClick={handleUser}>START!</button>
+					<button className="btn btn-success mt-3" onClick={handleUser}>START!</button>
 				</div>
-			</div>
+			</div>}
 
-			<form onSubmit={handleSubmit} className="form w-50 mx-auto">
+			{todos && <form onSubmit={handleSubmit} className="form w-50 mx-auto">
 				{/* we need the username, then we pass the username as props to the function to always receive a capitalize name */}
 				<h1>{username && capitalizeFirstLetter(username)} To-Do's</h1>
 				<input className="form-control" onChange={e => setTask(e.target.value)} placeholder="Add something" />
@@ -209,7 +237,7 @@ const Home = () => {
 					{/* todos && todos.map means that if there's information on todos, then do the map. remember we need to wait for the data to be fetched */}
 					{todos && todos.map((el, i) => {
 						return (
-							<div>
+							<div key={el.id}>
 								{/* we check if editing value is the same as the id from the element*/}
 								{editing == el.id ?
 									< div key={el.id} className="input-group d-flex justify-content-space-between"
@@ -227,8 +255,14 @@ const Home = () => {
 									<div>
 
 										{/* // if is not the id from the el === to the one on the state, we return a normal li */}
-										<li className="list-group-item  d-flex justify-content-between">{el.label}
+										<li className={`list-group-item  d-flex justify-content-between ${el.is_done ? 'text-decoration-line-through border-danger' : ''}`}>{el.label}
 											<span className="mx-3">
+												{el.is_done ?
+													<i className="fa-solid fa-square-check" onClick={() => handleNotDone(el, i)}></i>
+													:
+
+													<i className="fa-regular fa-square" onClick={() => handleDone(el, i)}></i>
+												}
 												{/* we set the id on the editing state to be able to edit the element */}
 												<i className="fa-solid fa-pen mx-3" onClick={() => setEditing(el.id)}></i>
 												{/* we send the id to the handleDelete function to be able to tell the api which element we want to delete */}
@@ -240,7 +274,7 @@ const Home = () => {
 							</div>)
 					})}
 				</ul >
-			</form >
+			</form >}
 
 
 
